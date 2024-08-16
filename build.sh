a4d7a4df3628143e273e9daab0f0d7beea64124d
Rheydskey <matgag@tutanota.com>
2024-08-16 16:46:35 +0200

    Update limine

--- build.sh
+++ build.sh
@@ -6,6 +6,14 @@ test $? -eq 0 || exit;
 
 bash ./meta/scripts/makeiso.sh
 
+if [ -f "/usr/share/edk2-ovmf/x64/OVMF_CODE.fd" ]
+then
+  file="/usr/share/edk2-ovmf/x64/OVMF_CODE.fd"
+else
+  file="/usr/share/edk2-ovmf/OVMF_CODE.fd"
+fi
+
+
 qemu-system-x86_64 -serial stdio \
                    -drive format=raw,file=zros.hdd \
                    -no-reboot \
@@ -13,7 +21,7 @@ qemu-system-x86_64 -serial stdio \
                    -m 1024M \
                    -M q35 \
                    -smp 4 \
-                   -bios /usr/share/edk2-ovmf/OVMF_CODE.fd \
+                   -bios $file \
                    -enable-kvm
                    # -d int,cpu_reset,in_asm \
                    # -s -S
--- meta/scripts/makeiso.sh
+++ meta/scripts/makeiso.sh
@@ -4,10 +4,11 @@ PWD=$(pwd)
 
 KERNEL_HDD="$PWD/zros.hdd"
 
+rm -rvf "$PWD/iso_root"
 
 mkdir -p "$PWD/iso_root"
 
-cp -v "$PWD/zig-out/bin/zros" "$PWD/bin/limine.cfg" "$PWD/limine/limine-bios.sys" "$PWD/limine/limine-bios-cd.bin" "$PWD/limine/limine-uefi-cd.bin" "$PWD/iso_root/"
+cp -v "$PWD/zig-out/bin/zros" "$PWD/bin/limine.conf" "$PWD/limine/limine-bios.sys" "$PWD/limine/limine-bios-cd.bin" "$PWD/limine/limine-uefi-cd.bin" "$PWD/iso_root/"
 
 xorriso -as mkisofs -b limine-bios-cd.bin \
 -no-emul-boot -boot-load-size 4 -boot-info-table \
