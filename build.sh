0287764addb6bef91d8462818a1e4c5d44cc20e8
Rheydskey <matgag@tutanota.com>
2024-09-13 10:00:13 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/env bash
+
+export DEBUG=0
+export KVM=0
+export GDB=0
+
 
 zig build $1
 
@@ -13,15 +18,32 @@ else
   file="/usr/share/edk2-ovmf/OVMF_CODE.fd"
 fi
 
-
-qemu-system-x86_64 -serial mon:stdio \
+export ARGS="-serial mon:stdio \
                    -drive format=raw,file=zros.hdd \
                    -no-reboot \
                    -no-shutdown \
                    -m 1024M \
                    -M q35 \
                    -smp 4 \
-                   -bios $file \
-                   # -d int,cpu_reset,in_asm \
-                   # -enable-kvm
-                   # -s -S
+                   -bios $file"
+
+if [[ $DEBUG -eq 1 ]]
+then
+export ARGS="$ARGS -d int,cpu_reset,in_asm"
+fi
+
+
+if [[ $KVM -eq 1 ]]
+then
+export ARGS="$ARGS -enable-kvm"
+fi
+
+
+if [[ $GDB -eq 1 ]]
+then
+export ARGS="$ARGS -s -S"
+fi
+
+echo $ARGS
+
+qemu-system-x86_64 $ARGS
